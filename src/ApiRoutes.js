c0ea6ac5cf7c3eb35741ed12689da14f66ebcfdd
Tom Gleitsmann <35902221+tmgleitsmann@users.noreply.github.com>
2020-08-28 09:54:05 -0500

    updated API Routes

--- src/ApiRoutes.js
+++ src/ApiRoutes.js
@@ -1,7 +1,16 @@
-export const postPlayers = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/soccersearch-redkv/service/dream_team_service/incoming_webhook/team-player-post';
-export const basicApiUrl = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/soccersearch-redkv/service/get-player-basic/incoming_webhook/get-player-basic';
-export const autocompleteApiUrl = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/soccersearch-redkv/service/get-player-basic/incoming_webhook/get-player-autocomplete';
-export const fuzzyApiUrl = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/soccersearch-redkv/service/get-player-basic/incoming_webhook/get-player-fuzzy';
-export const wildcardApiUrl = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/soccersearch-redkv/service/get-player-basic/incoming_webhook/get-player-wildcard';
-export const removePlayers = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/soccersearch-redkv/service/dream_team_service/incoming_webhook/team-player-remove';
-export const getPlayers = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/soccersearch-redkv/service/dream_team_service/incoming_webhook/get-team ';
+// export const postPlayers = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/soccersearch-redkv/service/dream_team_service/incoming_webhook/team-player-post';
+// export const basicApiUrl = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/soccersearch-redkv/service/get-player-basic/incoming_webhook/get-player-basic';
+// export const autocompleteApiUrl = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/soccersearch-redkv/service/get-player-basic/incoming_webhook/get-player-autocomplete';
+// export const fuzzyApiUrl = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/soccersearch-redkv/service/get-player-basic/incoming_webhook/get-player-fuzzy';
+// export const wildcardApiUrl = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/soccersearch-redkv/service/get-player-basic/incoming_webhook/get-player-wildcard';
+// export const removePlayers = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/soccersearch-redkv/service/dream_team_service/incoming_webhook/team-player-remove';
+//export const getPlayers = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/soccersearch-redkv/service/dream_team_service/incoming_webhook/get-team';
+
+export const postPlayers = 'https://webhooks.mongodb-realm.com/api/client/v2.0/app/fifa20_search-qazxh/service/dream_team_service/incoming_webhook/team-player-post';
+export const basicApiUrl = 'https://webhooks.mongodb-realm.com/api/client/v2.0/app/fifa20_search-qazxh/service/FindPlayers/incoming_webhook/get-player-basic';
+export const autocompleteApiUrl = 'https://webhooks.mongodb-realm.com/api/client/v2.0/app/fifa20_search-qazxh/service/FindPlayers/incoming_webhook/get-player-autocomplete';
+export const fuzzyApiUrl = 'https://webhooks.mongodb-realm.com/api/client/v2.0/app/fifa20_search-qazxh/service/FindPlayers/incoming_webhook/get-player-fuzzy';
+export const wildcardApiUrl = 'https://webhooks.mongodb-realm.com/api/client/v2.0/app/fifa20_search-qazxh/service/FindPlayers/incoming_webhook/get-player-wildcard';
+export const removePlayers = 'https://webhooks.mongodb-realm.com/api/client/v2.0/app/fifa20_search-qazxh/service/dream_team_service/incoming_webhook/team-player-remove';
+export const getPlayers = 'https://webhooks.mongodb-realm.com/api/client/v2.0/app/fifa20_search-qazxh/service/dream_team_service/incoming_webhook/get-team';
+
